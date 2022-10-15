9144235ef61b2333b9cf1e460810e97ee30024b6
Stacey Friesen <stacey@coldsnakedigital.com>
2022-10-14 17:40:03 -0700

    added EAV scope for querying values

--- src/Scopes/EntityAttributeValueScopes.php
+++ src/Scopes/EntityAttributeValueScopes.php
@@ -29,4 +29,25 @@ trait EntityAttributeValueScopes
             $query->where('entity_attribute_id', $attributeId);
         });
     }
+
+    /**
+     * Get instances with a given entity, attribute and value
+     *
+     * @param Builder $query
+     * @param $entity
+     * @param $attribute
+     * @param $value
+     *
+     * @return Builder
+     */
+    public function scopeWithEAV(Builder $query, $entity, $attribute, $value): Builder
+    {
+        $attributeId = ($attribute instanceof EntityAttribute) ? $attribute->id : $attribute;
+
+        $entityId = (isset($entity->id)) ? $entity->id : $entity;
+
+        return $query->whereHas(static::$relationName, function (Builder $query) use ($entityId, $attributeId, $value) {
+            $query->where('entity_id', $entityId)->where('entity_attribute_id', $attributeId)->where('value', 'LIKE', '%'.$value.'%');
+        });
+    }
 }
